de89fb8b0d1ebe8e67b73d42fa2c928b73a4d180
Ashantih2 <ashantihill2@gmail.com>
2026-05-17 11:53:14 -0400

    Committed first java assignment - PalmerPenguins
    used VM Horizons

---
+++ PalmerPenguins.java
@@ -0,0 +1,27 @@
+// PalmerPenguins.java
+// Ashanti Hill
+// May 17, 2026
+// Explore Palmer Penguins Dataset
+
+public class PalmerPenguins {
+
+    // constants to represent the species and count
+    static final String SP_CHINSTRAP = "Chinstrap";
+    static final String SP_GENTOO = "Gentoo";
+    static final String SP_ADELIE = "Adelie";
+    
+    static final int TOTAL_SPECIES =3;
+
+    public static void main(String[] args) {
+    
+    // output the species names with intro text
+    System.out.println("Introducing the Palmer Penguins: ");
+    System.out.println("\t" + SP_CHINSTRAP + "!");
+    System.out.println("\t" + SP_GENTOO + "!");
+    System.out.println("and last but not least...");
+    System.out.println("\t" + SP_ADELIE + "!");
+    System.out.println("There are a total of " + TOTAL_SPECIES +
+                       "penguin species in this dataset.");
+
+    }
+}
